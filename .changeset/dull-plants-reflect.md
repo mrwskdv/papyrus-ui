ee801ca2e4304c78572ade9215e4fa1e4dc18db2
Sergey Murawsky <mrwskdv@gmail.com>
2025-05-12 23:44:47 +0400

    feat(Image): Extend properties and add custom element support

---
+++ .changeset/dull-plants-reflect.md
@@ -0,0 +1,5 @@
+---
+'@papyrus-ui/components': patch
+---
+
+Extended Image properties and added custom element support
--- packages/components/src/components/image/image.tsx
+++ packages/components/src/components/image/image.tsx
@@ -3,26 +3,39 @@ import {
   atoms,
   MarginAtoms,
   partitionAtoms,
+  PositionAtoms,
+  ResponsiveValue,
+  RoundedAtoms,
   SizingAtoms,
 } from '@papyrus-ui/styles';
 import cn from 'classnames';
-import { FC, ImgHTMLAttributes } from 'react';
+import { cloneElement, FC, ImgHTMLAttributes, ReactElement } from 'react';
 
 export interface ImageProps
-  extends SizingAtoms,
+  extends PositionAtoms,
+    RoundedAtoms,
+    SizingAtoms,
     MarginAtoms,
     ImgHTMLAttributes<HTMLImageElement> {
+  alt: string;
+  display?: ResponsiveValue<'block' | 'inline-block' | 'none'>;
   objectFit?: Atoms['objectFit'];
+  src: string;
+  children?: ReactElement<ImgHTMLAttributes<HTMLImageElement>>;
 }
 
-export const Image: FC<ImageProps> = ({ alt, className, ...props }) => {
+export const Image: FC<ImageProps> = ({
+  alt,
+  className,
+  children,
+  ...props
+}) => {
   const [atomsProps, restProps] = partitionAtoms(props);
+  const classNames = cn(atoms(atomsProps), className);
 
-  return (
-    <img
-      alt={alt}
-      className={cn(atoms(atomsProps), className)}
-      {...restProps}
-    />
+  return children ? (
+    cloneElement(children, { ...restProps, alt, className: classNames })
+  ) : (
+    <img {...restProps} alt={alt} className={classNames} />
   );
 };
